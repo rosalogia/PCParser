2d0bca5b18416e37af0053b733a51c20840de64e
Ambika Nair <cepheac@protonmail.ch>
2020-09-23 02:47:23 -0400

    Separated Interpreter from Parser; Bare minimum Interpreter Implementation

---
+++ src/Interpreter/Program.fs
@@ -0,0 +1,134 @@
+﻿open PCParser.Parser
+open System
+
+type ProgramState = {VariableTable: (Name * Value) list; Operations: int}
+
+let lookupVariable state name =
+    state.VariableTable
+    |> List.filter (fun (n, _) -> n = name)
+    |> List.map snd
+    |> List.head
+
+let incrementOps state = {state with Operations = state.Operations + 1}
+
+let printValue value =
+    match value with
+    | Integer i         -> printfn "%i" i
+    | Bool b            -> printfn "%b" b
+    | Value.String s    -> printfn "%s" s
+
+let interpretConditionalValue value =
+    match value with
+    | Bool b    -> b
+    | _         -> true
+
+let add x y =
+    match x with
+    | Integer a     ->  match y with
+                        | Integer b         -> Integer (a + b)
+                        | Value.String b    -> Value.String (sprintf "%i%s" a b)
+    | Value.String a->  match y with
+                        | Integer b         -> Value.String (sprintf "%s%i" a b)
+                        | Value.String b    -> Value.String (sprintf "%s%s" a b)
+
+let compare comparison x y =
+    match x with
+    | Integer a     ->  match y with
+                        | Integer b     -> Bool (comparison a b)
+
+let comparator f x y =
+    match x with
+    | Bool a    ->  match y with
+                    | Bool b    -> Bool (f a b)
+
+let andOp = comparator (&&)
+let orOp = comparator (||)
+
+let gt = compare (>)
+let lt = compare (<)
+
+let mapOperator op =
+    match op with
+    | ADD   -> add
+    | GT    -> gt
+    | LT    -> lt
+    | AND   -> andOp
+    | OR    -> orOp
+
+let rec applyOperator state op e1 e2 =
+    let reduceToValue exp =
+        match exp with
+        | Literal value         -> value
+        | Variable name         -> lookupVariable state name
+        | Operation (x, op', y) -> applyOperator state op' x y
+    
+    mapOperator op (reduceToValue e1) (reduceToValue e2)
+
+
+let rec evaluateExpression state = function
+    | Literal value -> value
+    | Variable name -> lookupVariable state name
+    | Operation (e1, op, e2) -> applyOperator state op e1 e2
+
+let rec runStatement state s =
+    match s with
+    | DISPLAY exp ->
+        printValue (evaluateExpression state exp)
+        incrementOps state
+    | SET (name, exp) ->
+        let eValue = evaluateExpression state exp
+        incrementOps {state with VariableTable = (name, eValue) :: state.VariableTable}
+    | READ name ->
+        let input = Console.ReadLine()
+        
+        let (|Int|_|) input =
+           match System.Int32.TryParse(input:string) with
+           | (true,int) -> Some(int)
+           | _ -> None
+
+        let (|Bool|_|) input =
+           match System.Boolean.TryParse(input:string) with
+           | (true,bool) -> Some(bool)
+           | _ -> None
+        
+        let value = match input with
+                    | Int i     -> Integer i
+                    | Bool b    -> Bool b
+                    | _         -> Value.String input
+        
+        incrementOps {state with VariableTable = (name, value) :: state.VariableTable}
+    | IF (cond, _, None) when not (cond |> evaluateExpression state |> interpretConditionalValue) ->
+        incrementOps state
+    | IF (cond, _, Some (elseBlock)) when not (cond |> evaluateExpression state |> interpretConditionalValue) ->
+        runStatements elseBlock (incrementOps state)
+    | IF (_, block, _)  ->
+        runStatements block (incrementOps state)
+
+and runStatements statements state =
+    match statements with
+    | [] -> state
+    | [HALT] -> state
+    | [s] ->
+        runStatement state s
+    | h::t ->
+        runStatement state h
+        |> runStatements t
+
+
+
+let runProgram statements state =
+    let finalState = runStatements statements state
+    printfn "Operations: %i" finalState.Operations
+    0
+
+[<EntryPoint>]
+let main argv =
+    argv
+    |> String.concat "+"
+    |> fun path ->  match parsePCFile path with
+                    | Ok result ->
+                        printfn "Success: %A" result
+                        runProgram result {VariableTable = []; Operations = 0}
+                    | Error err ->
+                        printfn "Error: %A" err
+                        1
---
+++ src/Parser/Library.fs
@@ -0,0 +1,211 @@
+﻿namespace PCParser
+
+module Parser =
+
+    open FParsec
+    open System.Text
+
+    type Name = string
+
+    type Value =
+        | Integer   of int
+        | String    of string
+        | Bool      of bool
+
+    type Operator =
+        | ADD
+        | SUBTRACT
+        | MULTIPLY
+        | DIVIDE
+        | MODULUS
+        | GT
+        | LT
+        | GTE
+        | LTE
+        | EQUALS
+        | NOTEQUALS
+        | NOT
+        | AND
+        | OR
+    type Expr =
+        | Literal   of Value
+        | Variable  of Name
+        | Operation of Expr * Operator * Expr
+
+    type Statement =
+        | READ      of Name
+        | DISPLAY   of Expr
+        | SET       of Name * Expr
+        | COMPUTE   of Name * Expr
+        | IF        of Expr * Block * Block option
+        | WHILE     of Expr * Block
+        | DOWHILE   of Block * Expr
+        | REPEAT    of Block * Expr
+        | HALT
+    and Block = Statement list
+
+    let pword s = pstring s .>> spaces
+
+    let pidentifier: Parser<string, Unit> =
+        many1Satisfy2 (System.Char.IsLetter) (System.Char.IsWhiteSpace >> not)
+        .>> spaces
+
+    let pread: Parser<Statement, Unit> =
+        pword "READ"
+        >>. pidentifier
+        |>> READ
+
+    let pbool: Parser<bool, Unit> =
+        pstring "true" <|> pstring "false"
+        |>> System.Boolean.Parse
+
+    let pstringliteral: Parser<string, Unit> =
+        let words = pchar '\"' >>. manyCharsTill anyChar (pchar '\"')
+        words |>> string .>> spaces
+
+    let pintval: Parser<Value, Unit>    =   pint64          |>> int     |>> Integer
+    let pstringval: Parser<Value, Unit> =   pstringliteral  |>> String
+    let pboolval: Parser<Value, Unit>   =   pbool           |>> Bool
+
+    let pvalue =
+        choice [
+            pintval
+            pstringval
+            pboolval
+        ]
+
+    let createOperation op x y = Operation (x, op, y)
+
+    let pvariable   = pidentifier |>> Variable
+    let pliteral    = pvalue |>> Literal
+
+    // This is safe, since the parser will never pass non-Integers to this function
+    let intOperatorParser = new OperatorPrecedenceParser<Expr,Unit,Unit>()
+    let intexpr = intOperatorParser.ExpressionParser
+    let intterm = (pintval .>> spaces |>> Literal <|> pvariable) <|> between (pword "(") (pword ")") intexpr
+    intOperatorParser.TermParser <- intterm
+
+    intOperatorParser.AddOperator(InfixOperator(">",    spaces, 1, Associativity.Left, (createOperation GT)))
+    intOperatorParser.AddOperator(InfixOperator("<",    spaces, 1, Associativity.Left, (createOperation LT)))
+    intOperatorParser.AddOperator(InfixOperator(">=",   spaces, 1, Associativity.Left, (createOperation GTE)))
+    intOperatorParser.AddOperator(InfixOperator("<=",   spaces, 1, Associativity.Left, (createOperation LTE)))
+    intOperatorParser.AddOperator(InfixOperator("==",   spaces, 1, Associativity.Left, (createOperation EQUALS)))
+    intOperatorParser.AddOperator(InfixOperator("!=",   spaces, 1, Associativity.Left, (createOperation NOTEQUALS)))
+    intOperatorParser.AddOperator(InfixOperator("+",    spaces, 2, Associativity.Left, (createOperation ADD)))
+    intOperatorParser.AddOperator(InfixOperator("-",    spaces, 2, Associativity.Left, (createOperation SUBTRACT)))
+    intOperatorParser.AddOperator(InfixOperator("*",    spaces, 3, Associativity.Left, (createOperation MULTIPLY)))
+    intOperatorParser.AddOperator(InfixOperator("/",    spaces, 3, Associativity.Left, (createOperation DIVIDE)))
+    intOperatorParser.AddOperator(InfixOperator("MODULUS",  spaces, 3, Associativity.Left, (createOperation MODULUS)))
+
+    let boolOperatorParser = new OperatorPrecedenceParser<Expr,Unit,Unit>()
+    let boolexpr = boolOperatorParser.ExpressionParser
+    let boolterm = (pboolval .>> spaces |>> Literal <|> intexpr) <|> between (pword "(") (pword ")") boolexpr
+    boolOperatorParser.TermParser <- boolterm
+
+    boolOperatorParser.AddOperator(InfixOperator("AND", spaces, 1, Associativity.Left, (createOperation AND)))
+    boolOperatorParser.AddOperator(InfixOperator("OR",  spaces, 1, Associativity.Left, (createOperation OR)))
+    // boolOperatorParser.AddOperator(PrefixOperator("NOT",spaces, 1, false, function (Bool p) -> Bool (not p)))
+
+
+    let poperation  = choice [ boolexpr ; intexpr ]
+
+    let rec pexpression: Parser<Expr, Unit> = choice [poperation ; pliteral ; pvariable]
+
+    let pstatement, pstatementref = createParserForwardedToRef<Statement, Unit>()
+
+    let pdisplay: Parser<Statement, Unit> =
+        pword "DISPLAY"
+        >>. pexpression
+        |>> DISPLAY
+
+    let pset: Parser<Statement, Unit> =
+        let ident = between (pword "SET") (pword "AS") pidentifier
+
+        let setval = pexpression
+
+        pipe2 ident setval (fun name value -> SET (name, value))
+
+    let pcompute: Parser<Statement, Unit> =
+        let ident = between (pword "COMPUTE") (pword "AS") pidentifier
+
+        pipe2 ident pexpression (fun name expression -> COMPUTE (name, expression))
+
+    let pif: Parser<Statement, Unit> =
+        let parseElseOrEnd =
+            (pstring "ELSE" <|> pstring "ENDIF")
+            |> followedBy
+            .>> spaces
+        
+        let parseElse =
+            pword "ELSE"
+            >>. manyTill (pstatement .>> spaces) (pword "ENDIF")
+
+        let condition = between (pword "IF") (pword "THEN") pexpression
+        let inner1 = manyTill (pstatement .>> spaces) parseElseOrEnd
+
+        pipe3 condition inner1 (opt parseElse) (fun cond in1 in2 -> IF (cond, in1, in2))
+
+    let pwhile: Parser<Statement, Unit> =
+        let condition =
+            (pword "WHILE")
+            >>. between (pstring "(") (pstring ")") pexpression
+        let inner = manyTill (pstatement .>> spaces) (pword "ENDWHILE")
+
+        pipe2 condition inner (fun cond block -> WHILE (cond, block))
+
+    let pdowhile: Parser<Statement, Unit> =
+        let inner =
+            (pword "DO")
+            >>. manyTill (pstatement .>> spaces) (pword "WHILE")
+        
+        let condition = between (pstring "(") (pstring ")") pexpression
+
+        pipe2 inner condition (fun block cond -> DOWHILE (block, cond))
+
+    let prepeat: Parser<Statement, Unit> =
+        let inner =
+            (pword "REPEAT")
+            >>. manyTill (pstatement .>> spaces) (pword "UNTIL")
+        
+        let condition = between (pstring "(") (pstring ")") pexpression
+
+        pipe2 inner condition (fun block cond -> REPEAT (block, cond))
+    
+    let phalt: Parser<Statement, Unit> =
+        pword "HALT"
+        >>. preturn HALT
+
+    do pstatementref := choice [
+        pread
+        pdisplay
+        pset
+        pcompute
+        pif
+        pwhile
+        pdowhile
+        prepeat
+        phalt
+    ]
+
+    let test p str =
+        match runParserOnFile p () str Encoding.ASCII with
+        | Success(result, _, _)   -> printfn "Success: %A" result
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+    let testSingle p str =
+        match run p str with
+        | Success (result, _, _) -> printfn "Success: %A" result
+        | Failure (error, _, _)  -> printfn "Failure: %s" error
+
+    let parsePCFile path =
+        match runParserOnFile (many pstatement) () path Encoding.ASCII with
+        | Success (result, _, _)    -> Core.Ok (result)
+        | Failure (error, _, _)     -> Core.Error (error)
+
+    (* [<EntryPoint>] *)
+    (* let main argv = *)
+    (*     argv *)
+    (*     |> String.concat "+" *)
+    (*     // |> testSingle pstatement *) 
+    (*     |> test (manyTill pstatement (pword "HALT")) *)
+    (*     0 *)
